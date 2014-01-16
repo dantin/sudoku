ba52573c25362e4ef0398c7e71e4c19ccbd5e9db
David Ding <chengjie.ding@gmail.com>
2014-01-17 06:48:48 +0800

    add implementation

--- sudoku.c
+++ sudoku.c
@@ -4,6 +4,8 @@
 
 #include "sudoku.h"
 
+#define SEP_LINE "+------+------+------+"
+
 // 数独矩阵
 static int matrix[ SIZE * SIZE ];
 
@@ -16,28 +18,112 @@ static int rows[ SIZE ];  // 列
 static int grids[ SIZE ]; // 网格
 
 /*
- * 打印数据矩阵内容
+ * 打印数独矩阵内容
  */
 static void print( void )
 {
+  int r, c;
 
+  for( r = 0; r < SIZE; r++ ) {
+    if( r % 3 == 0 ) {
+      printf( "%s\n", SEP_LINE );
+    }
+    for( c = 0; c < SIZE; c++ ) {
+      if( c % 3 == 0 ) {
+	printf( "|" );
+      }
+      int num = matrix[ r * SIZE + c ];
+      if( num == 0 ) {
+	printf( ". ");
+      } else {
+	printf( "%d ", num );
+      }
+    }
+    printf( "|\n" );
+  }
+
+  printf( "%s\n\n", SEP_LINE );
 }
 
+/*
+ * 在数独矩阵中找到第一个待解元素的序号，找不到返回-1
+ */
 static int find( void )
 {
+  int i;
+
+  for( i = 0; i < SIZE * SIZE; i++ ) {
+    if( matrix[i] == 0 ) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
+/*
+ * 设置数独矩阵中序号为loc的值为num
+ *
+ *   成功返回true
+ *   失败返回false
+ */
 static bool set( const int loc, const int num )
 {
-  return false;
+  int r = loc / SIZE;
+  int c = loc % SIZE;
+  int g = ( r / 3 ) * 3 + c / 3;
+
+  bool valid = matrix[loc] == 0
+    && ( cols[c] & ( 1 << num ) ) == 0
+    && ( rows[r] & ( 1 << num ) ) == 0
+    && ( grids[g] & ( 1 << num ) ) == 0;
+
+  if( !valid ) {
+    return false;
+  }
+
+  matrix[loc] = num;
+  cols[c] |= ( 1 << num );
+  rows[r] |= ( 1 << num );
+  grids[g] |= ( 1 << num );
+
+  return true;
 }
 
+/*
+ * 清楚数独矩阵中序号为loc的值
+ */
 static void unset( const int loc )
 {
+  int r = loc / SIZE;
+  int c = loc % SIZE;
+  int g = ( r / 3 ) * 3 + c / 3;
+
+  int num = matrix[loc];
+  matrix[loc] = 0;
+  cols[c] ^= ( 1 << num );
+  rows[r] ^= ( 1 << num );
+  grids[g] ^= ( 1 << num );
 }
 
+/*
+ * 计算数独并显示结果
+ */
 void sudoku( void )
 {
+  int loc = find();
+  if( loc < 0 ) {
+    print();
+    return;
+  }
+
+  int n;
+  for( n = 1; n < 10; n++ ) {
+    if( set( loc, n ) ) {
+      sudoku();
+      unset( loc );
+    }
+  }
 }
 
 /*
@@ -56,5 +142,27 @@ void sudoku( void )
  */
 void read( void )
 {
+  int loc, c;
 
+  for( loc = 0; loc < SIZE * SIZE; ) {
+    c = getchar();
+
+    if( c == EOF ) {
+      break;
+    }
+
+    if( c == '#' ) {
+      while( ( c = getchar() ) && c >= 0 && c != '\n' && c != '\r' ) {
+	;
+      }
+    } else if( c >= '1' && c <= '9' ) {
+      set( loc, c - '0' );
+      loc++;
+    } else if( c == '.' || c == '0' ) {
+      loc++;
+    }
+  }
+
+  printf( "Sudoku Game:\n" );
+  print();
 }
